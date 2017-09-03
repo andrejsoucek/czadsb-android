906c575aa4b7571535004d57e774d08ade801d38
Andrej Souček <andrej.soucek@antee.cz>
2017-09-03 22:49:19 +0200

    Certificate trust work-around deleted

--- src/main/java/cz/adsb/czadsb/PlanesFetcher.kt
+++ src/main/java/cz/adsb/czadsb/PlanesFetcher.kt
@@ -2,12 +2,9 @@ package cz.adsb.czadsb
 
 import com.github.kittinunf.fuel.Fuel
 import cz.adsb.czadsb.model.AircraftList
-import java.security.SecureRandom
-import java.security.cert.X509Certificate
-import javax.net.ssl.*
 
 object PlanesFetcher {
-    private val URL = "https://czadsb.cz/private/AircraftList.json"
+    private val URL = "https://czadsb.cz/public/AircraftList.json"
 
     fun fetchAircrafts(north: Double, south: Double, west: Double, east: Double): AircraftList {
         var aircraftList: AircraftList? = null
@@ -21,7 +18,6 @@ object PlanesFetcher {
     }
 
     private fun fetchFull(north: Double, south: Double, west: Double, east: Double): AircraftList? {
-        trustEveryone()
         val (_, _, result) = Fuel.post("$URL?fNbnd=$north&fSBnd=$south&fWbnd=$west&fEBnd=$east")
             .responseObject(AircraftList.Deserializer())
         val (aircraftList, err) = result
@@ -29,30 +25,9 @@ object PlanesFetcher {
     }
 
     private fun fetchChanges(ldv: String, north: Double, south: Double, west: Double, east: Double): AircraftList? {
-        trustEveryone()
         val (_, _, result) = Fuel.post("$URL?ldv=$ldv&fNbnd=$north&fSBnd=$south&fWbnd=$west&fEBnd=$east")
                 .responseObject(AircraftList.Deserializer())
         val (aircraftList, err) = result
         return aircraftList
-    }
-
-    private fun trustEveryone() {
-        try {
-            HttpsURLConnection.setDefaultHostnameVerifier { hostname, session -> true }
-            val context = SSLContext.getInstance("TLS")
-            context.init(null, arrayOf<X509TrustManager>(object: X509TrustManager {
-                override fun checkClientTrusted(chain:Array<X509Certificate>,
-                                       authType:String) {}
-                override fun checkServerTrusted(chain:Array<X509Certificate>,
-                                       authType:String) {}
-                override fun getAcceptedIssuers(): Array<X509Certificate?> =
-                        arrayOfNulls<X509Certificate>(0)
-            }), SecureRandom())
-            HttpsURLConnection.setDefaultSSLSocketFactory(
-                    context.getSocketFactory())
-        }
-        catch (e:Exception) { // should never happen
-            e.printStackTrace()
-        }
     }
 }
